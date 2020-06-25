421db267b18642f96042cc4fe2a210e8e163b2dc
Adam Miller <theadammiller56@gmail.com>
2020-06-25 09:34:00 -0500

    Added Details

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/BarrelsController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/BarrelsController.cs
@@ -13,5 +13,9 @@ namespace CTWMasterClass_WebAppActivities.Controllers
         {
             return View();
         }
+        public ActionResult Details()
+        {
+            return View();
+        }
     }
 }
