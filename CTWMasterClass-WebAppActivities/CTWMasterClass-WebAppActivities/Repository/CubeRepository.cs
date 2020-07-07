6c3494979bcb91f3205c170a8b5c62f642f2987a
Ruilin Yan <ry6989@stu.nbexcellence.org>
2020-07-07 10:36:03 -0500

    Create and Edit Cubes views added

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Repository/CubeRepository.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Repository/CubeRepository.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using CTWMasterClass_WebAppActivities.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace CTWMasterClass_WebAppActivities.Repository
+namespace CTWMasterClass_WebAppActivities.Repositories
 {
     public class CubeRepository
     {
+        private ApplicationDbContext dbContext;
+
+        public CubeRepository()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        public List<Cube> GetAllCubes()
+        {
+            return dbContext.Cubes.ToList();
+        }
+
+        public void AddCube(Cube toAdd)
+        {
+            dbContext.Cubes.Add(toAdd);
+            dbContext.SaveChanges();
+        }
+        public Cube GetCubeById(int id)
+        {
+            return dbContext.Cubes.Find(id);
+        }
+        public void SaveEdits(Cube toSave)
+        {
+            dbContext.Entry(toSave).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteCube(Cube toDelete)
+        {
+            dbContext.Cubes.Remove(toDelete);
+            dbContext.SaveChanges();
+        }
     }
 }
