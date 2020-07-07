0e0f548411deca5cfdfddebd13320f580b897377
Andrei <andrei.v.tarakanov@gmail.com>
2020-07-07 11:06:05 -0500

    New View Folder Changes

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CubeController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CubeController.cs
@@ -5,7 +5,12 @@ using System.Web;
 
 namespace CTWMasterClass_WebAppActivities.Controllers
 {
-    public class CubeController
+    public class CubeController : Controller
     {
+        private CubeService service = new CubeService();
+        public ActionResult Index()
+        {
+            return View(service.GetAllCubes());
+        }
     }
 }
